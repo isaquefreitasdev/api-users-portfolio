2e6141f62d03c6c49afc298751862819fc16de4d
Isaque <freitasmelo.mkt.online@gmail.com>
2024-09-17 09:33:40 -0300

    vercel

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log("data")
-});
-
-const routes = require("./routes/routes")
 const cors = require("cors");
+const routes = require("./routes/routes");
 
+// Conectando ao MongoDB
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
 
-app.use(cors())
-app.use("/",express.json(),routes);
+// Middlewares
+app.use(cors());
+app.use("/", express.json(), routes);
 
-
-
-
-app.listen(3001, () => {
-    console.log("Lendo")
-})
+// Exporta o app para a Vercel tratar
+module.exports = app;
